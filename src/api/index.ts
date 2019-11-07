02d810b21655f778e84fa504b4c1a6492ca85ba1
tuominen-aleksi <tuominen-aleksi@dmm.com>
2019-11-07 20:15:53 +0900

    Remove stuff

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const createInstance = () => {
   const app = axios.create()
-  app.defaults.baseURL = process.env.BASE_URL || 'http://localhost:8080'
+  app.defaults.baseURL = process.env.BASE_URL
   app.interceptors.response.use(({ data }) => data)
   app.interceptors.request.use((config) => {
     if (config.method !== 'get') {
